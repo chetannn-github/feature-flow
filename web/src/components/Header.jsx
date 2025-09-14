9c10de66d169c71a316132d46e119ad275900ab3
chetannn-github <chetan.rajawat25@gmail.com>
2025-09-14 12:56:50 +0530

    fix: enhance header menu for mobile

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -1,21 +1,35 @@
 import { Button } from "./ui/button";
-import { Flag, Menu } from "lucide-react";
-import { useState } from "react";
+import { Cross, Flag, Menu, X } from "lucide-react";
+import { useEffect, useState } from "react";
 import { ThemeToggle } from "./ThemeToggle";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from '../store/authSlice'
+import { useTheme } from "./ThemeProvider";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const auth = useSelector(s => s.auth);
   const dispatch = useDispatch();
+  const closeMenu = () => { setIsMenuOpen(false) }
+  const { theme, setTheme } = useTheme();
 
+  useEffect(() => {
+    const handleDocumentClick = (event) => {
+        closeMenu();
+    };
+    document.getElementsByTagName("main")[0].addEventListener('click', handleDocumentClick);
+    return () => {
+        document.getElementsByTagName("main")[0].removeEventListener('click', handleDocumentClick);
+    };
+  },[]); 
+
+  
   return (
     <header className="fixed top-0 mb-16 left-0 right-0 z-50 bg-background/80 backdrop-blur-md border-b border-border">
       <div className="container mx-auto px-4">
-        <div className="flex items-center justify-between h-16">
-          <Link to={"/"}>
+        <div className="flex items-center justify-between h-16" >
+          <Link to={"/"} onClick={closeMenu}>
             <div className="flex items-center gap-2">
               <div className="flex items-center justify-center w-10 h-10 rounded-2xl bg-gradient-hero">
                 <Flag className="h-5 w-5 text-primary-foreground" />
@@ -27,7 +41,7 @@ const Header = () => {
 
           <div className="hidden md:flex items-center gap-4">
             <ThemeToggle />
-            {!auth?.token && <Link to={"/sign-in"}>
+            {!auth?.token && <Link to={"/sign-in"} >
               <Button variant="ghost">
                 Sign In
               </Button>
@@ -58,41 +72,44 @@ const Header = () => {
             className="md:hidden"
             onClick={() => setIsMenuOpen(!isMenuOpen)}
           >
-            <Menu className="h-5 w-5" />
+            {!isMenuOpen && <Menu className="h-5 w-5" />}
+            {isMenuOpen && <X className="h-5 w-5" />}
           </Button>
         </div>
 
         {isMenuOpen && (
           <div className="md:hidden py-4 border-t border-border">
             <nav className="flex flex-col gap-4">
-              <a href="#features" className="text-muted-foreground hover:text-foreground transition-colors">
-                Features
-              </a>
-              <a href="#docs" className="text-muted-foreground hover:text-foreground transition-colors">
-                Documentation
-              </a>
               <div className="flex flex-col gap-2 pt-4 border-t border-border">
+                <div className="flex gap-3 items-center" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
+                  <ThemeToggle />
+                  <div>Toggle Theme</div>
+                </div>
                 
 
-              {!auth?.token && <Link to={"/sign-in"}>
+              {!auth?.token && <Link to={"/sign-in"} onClick={closeMenu}>
                 <Button variant="ghost" className="justify-start w-full">
                   Sign In
                 </Button>
                 </Link>
               }
               
-              {!auth?.token &&<Link to={"/sign-up"}>
+              {!auth?.token &&<Link to={"/sign-up"} onClick={closeMenu}>
                 <Button className="justify-start w-full">
                   Get Started
                 </Button>
                 </Link>
               }
 
-              {auth?.token &&<div className="w-full" onClick={() => dispatch(logout())}>
-                <Button >
-                  Logout
-                </Button>
-            </div>}
+              {auth?.token &&<Link to={"/dashboard"} className="text-muted-foreground hover:text-foreground transition-colors" onClick={closeMenu}>
+                    <Button variant="ghost"> Go to Dashboard</Button>
+                </Link>
+              }
+
+              {auth?.token && <div className="w-full" onClick={() => dispatch(logout())} onClick={closeMenu}>
+                  <Button > Logout</Button>
+                </div>
+              }
                 
               </div>
             </nav>
